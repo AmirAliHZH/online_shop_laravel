e18185f36a2b4d95188a87698db44df77cf2872a
amirali <info@amiralih.ir>
2025-12-09 12:36:42 +0330

    migration user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('gender',1);
+            $table->string('code',6)->nullable();
+            $table->string('mobile_number',11);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
