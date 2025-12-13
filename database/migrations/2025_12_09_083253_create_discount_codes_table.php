3af61f022465c27ebc30e4361bae554408c22efa
amirali <info@amiralih.ir>
2025-12-13 13:50:01 +0330

    add migration discount_codes

--- database/migrations/2025_12_09_083253_create_discount_codes_table.php
+++ database/migrations/2025_12_09_083253_create_discount_codes_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('discount_codes', function (Blueprint $table) {
             $table->id();
+            $table->string('code')->unique(); // کد تخفیف
+            $table->enum('type', ['percent', 'fixed']); // درصدی یا عددی
+            $table->unsignedInteger('value'); // مقدار تخفیف
+            $table->unsignedInteger('max_discount')->nullable(); // سقف تخفیف
+            $table->timestamp('starts_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
