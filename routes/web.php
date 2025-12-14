729d6b9821cedad1a708bda7b023bce03f6695f0
amirali <info@amiralih.ir>
2025-12-14 12:53:02 +0330

    add route panel admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("/admin")->group(function () {
+    Route::get('/',[SiteController::class,"showAdminDashboard"]);
 });
